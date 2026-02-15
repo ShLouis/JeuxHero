cce13d08590507e0c4021df0feb57d61de38749d
lilia <lilianmaynadierodrigynov.comuez@>
2026-02-15 14:44:40 +0100

    fix mine

--- src/building/mine.py
+++ src/building/mine.py
@@ -1,17 +1,27 @@
-from building import Building
-from src.building.levelManager import LevelManager
-from src.character.hero import Hero
-from src.building.enemySpwaner import EnemySpawner
+from typing import List
+
+from building.building import Building
+from building.enemySpwaner import EnemySpawner
+from building.levelManager import LevelManager
+from character.hero import Hero
+from character.monster import Monster
 
 
 class Mine(Building):
     name = "Mine"
-    description = "In the Mine you can kill monsters to earn gold."
+    description = "A mine filled with monsters."
 
-    def __init__(self, ):
-        self.level_manager = LevelManager()
-        self.spawner = EnemySpawner(self)
+    def __init__(self,
+                 level_manager: LevelManager,
+                 spawner: EnemySpawner):
+        self.level_manager = level_manager
+        self.spawner = spawner
+        self.monsters: List[Monster] = []
+
+    def enter(self, hero: Hero) -> None:
+        print(f"You enter the {self.name}.")
+        current_level = self.level_manager.current_level
+        print(f"Current level: {current_level}")
+        nb_monsters = self.level_manager.get_nb_monsters()
+        print(f"number of monsters: {nb_monsters}")
         self.monsters = self.spawner.spawn_enemies()
-
-    def enter(self, hero: "Hero"):
-        ...
