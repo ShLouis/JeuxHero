71872649341ef97df13a1822ef0a4f884d6a598d
lilia <lilianmaynadierodrigynov.comuez@>
2026-02-19 12:59:50 +0100

    fix manabar

--- src/character/manaBar.py
+++ src/character/manaBar.py
@@ -4,7 +4,10 @@ class ManaBar:
         self.mana = max_mana
 
     def spendMana(self, amount: int):
+        if self.mana < amount:
+            return False
         self.mana = max(0, self.mana - amount)
+        return True
 
     def gainMana(self, amount: int):
         self.mana = min(self.max_mana, self.mana + amount)
